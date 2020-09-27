35bebc2d731b5b04d3bb272b9f1f8ec250e0eca6
Erik Badger <erikbadger777@gmail.com>
2020-09-27 14:24:20 -0500

    Revert "added express and handler"
    This reverts commit bea6b80b44299b26fa7ebc2ac0a9a625c9ca8818.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
-import serverless from 'serverless-http';
-import express from 'express';
+const handler = async () => {
+    const res = {
+        statusCode: 200,
+        body: JSON.stringify('Hello world aws lambda + ci/cd!!')
+    }
+    return res;
+}
 
-const app = express();
-app.use(express.urlencoded({ extended:true }));
-app.use(express.json());
-app.get('/', (req, res) => {
-    res.send({ msg:'Hello World' })
-})
-
-const handler = serverless(app);
-
-export { handler }
+export { handler };
